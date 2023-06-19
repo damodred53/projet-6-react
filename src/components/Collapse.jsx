fa5244640e06ba728f9b2fb479b2b7c17662c21a
Florent <florent.guyard@live.fr>
2023-06-19 12:01:49 +0200

    suite intégration page about

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -2,32 +2,12 @@ import derouleur from "../assets/derouleur.png";
 import { useState } from "react";
 
 
-
-
-
 function Collapse({titre, paragraphe, index,id}) {
-const [selected, setSelected] = useState(true);
+const [selected, setSelected] = useState(false);
 
     
 function handleClick() {
-    console.log(selected)
-    
-    if (selected === true) {
-      const paragraphs = document.querySelectorAll('.paragraphe_about');
-      paragraphs.forEach((paragraph) => {
-        
-       if (paragraph.id == id){
-        paragraph.style.display = 'block';
-        setSelected(false)
-    }});
-    } else {
-        
-      const paragraphs = document.querySelectorAll('.paragraphe_about');
-      paragraphs.forEach((paragraph, index) => {
-        paragraph.style.display = 'none';
-        setSelected(true);
-      });
-    }
+    setSelected(!selected);
   }
 
   
@@ -37,12 +17,19 @@ return (
         <li className="listing_about">
             <div  className="part_list_about">
                 <h2 className="title_about">{titre}</h2>
-                <img className="icon_about" id={id}  src={derouleur} alt="derouleur" onClick={() => handleClick({id})}/>
+                <img className={`icon_about ${selected ? 'active' : ''}`} id={id}  src={derouleur} alt="derouleur" onClick={handleClick}/>
             </div>
-                <p id={id} className="paragraphe_about">{paragraphe}</p>  
+            <div className={ selected ?  "div_about" : "null"}>
+            {selected && <p id={id} className="paragraphe_about" >{paragraphe}</p>}
+            </div> 
         </li>
     
     )
 }
+
+
 export default Collapse;
+
+
+
 
