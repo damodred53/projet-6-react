e7408253071deb30620fa1a848d0f306ce8f18ae
Florent <florent.guyard@live.fr>
2023-06-20 13:15:59 +0200

    fin de l'intégration home

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,11 @@ import dataHouse from "../data";
     return(
         dataHouse.map((e,id, index) => 
     (
-        <div className="card_logement" key={`${id}-${index}`} >      
-            <img className="image_background_card" src={e.cover} alt={e.title} key={`${id}-${index}`}/>
-            <p className="text_card">{e.title}</p>
+        <div className="card_logement" key={`${id}-${index}`} >  
+        
+            <div className="image_background_card" style={{ backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.6)), url(${e.cover})` }}   alt={e.title} key={`${id}-${index}`} />
+           
+            <p className="text_card">{e.title}</p> 
         </div>   
     )))
           
--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -8,20 +8,16 @@ const [selected, setSelected] = useState(false);
     
 function handleClick() {
     setSelected(!selected);
-  }
-
-  
-    
+  } 
 return (
     
         <li className="listing_about">
             <div  className="part_list_about">
                 <h2 className="title_about">{titre}</h2>
-                <img className={`icon_about ${selected ? 'active' : ''}`} id={id}  src={derouleur} alt="derouleur" onClick={handleClick}/>
+                <img className={`icon_about ${selected ? 'active' : 'inactive'}`} id={id}  src={derouleur} alt="derouleur" onClick={handleClick}/>
+                
             </div>
-            <div className={ selected ?  "div_about" : "null"}>
-            {selected && <p id={id} className="paragraphe_about" >{paragraphe}</p>}
-            </div> 
+            {selected && <div className="div_about"><p id={id} className="paragraphe_about " >{paragraphe}</p></div>}   
         </li>
     
     )
