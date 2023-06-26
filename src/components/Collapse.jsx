c563e3f052b184f9e724545bd06296273fde528b
Florent <florent.guyard@live.fr>
2023-06-26 23:29:41 +0200

    réglage de certain bugs

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,39 +1,54 @@
 import derouleur from "../assets/derouleur.png";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
-function Collapse({titre, paragraphe, id}) {
-const [selected, setSelected] = useState(false);
-   
+function Collapse({ titre, paragraphe, id }) {
+ 
+  const [selected, setSelected] = useState(false);
+  const [count, setCount] = useState(0);
   function handleClick() {
-      setSelected(!selected);
-    } 
-
-return (
     
+      setCount(count +1);
+    setSelected(!selected);
+  }
+
+  return (
     <li className="listing_about">
-    <div className="part_list_about">
-      <h2 className="title_about">{titre}</h2>
-      <img className={`icon_about  ${selected ? 'active' : 'inactive'}`} id={id} src={derouleur} alt="derouleur" onClick={handleClick} />
-    </div>
-    <div className={`div_about_supp ${!selected ? 'closing_animation' : ''}`}>
-      {selected && (
-        <div className="div_about">
-          {Array.isArray(paragraphe) ? (
-            paragraphe.map((paragraph, index) => (
-              <p id={id} key={index} 
-              className={`paragraphe_about ${Array.isArray(paragraphe) ? "multielement_about" : ''}`}>{paragraph}</p>
-            ))
-          ) : (
-            <p id={id} 
-            className={`paragraphe_about onlyoneelement_about ${!selected ? 'closing_animation' : ''}`}>{paragraphe}</p>
-          )}
-        </div>
-      )}
-    </div>
-  </li>
-    
-    )
+      <div className="part_list_about">
+        <h2 className="title_about">{titre}</h2>
+        <img
+          className={`icon_about ${selected ? 'active' : ''}`}
+          id={id}
+          src={derouleur}
+          alt="derouleur"
+          onClick={handleClick}
+        />
+      </div>
+      <div className={`div_about_supp ${!selected && count>0 ? 'closing_animation' : ''}`}>
+        {selected && (
+          <div className="div_about">
+            {Array.isArray(paragraphe) ? (
+              paragraphe.map((paragraph, index) => (
+                <p
+                  id={id}
+                  key={index}
+                  className={`paragraphe_about ${Array.isArray(paragraphe) ? "multielement_about" : ''}`}
+                >
+                  {paragraph}
+                </p>
+              ))
+            ) : (
+              <p
+                id={id}
+                className={`paragraphe_about onlyoneelement_about ${!selected ? 'closing_animation' : ''}`}
+              >
+                {paragraphe}
+              </p>
+            )}
+          </div>
+        )}
+      </div>
+    </li>
+  );
 }
-
 
 export default Collapse;
