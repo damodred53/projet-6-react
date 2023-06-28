1d342ec0af56f596b6cd68c4cce97ab0a01519fe
Florent <florent.guyard@live.fr>
2023-06-28 10:17:30 +0200

    réglages pour le collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,56 +1,57 @@
+import React, { useState, useEffect, useRef } from "react";
 import derouleur from "../assets/derouleur.png";
-import { useState} from "react";
 
 function Collapse({ titre, paragraphe, id }) {
- 
+  const ref = useRef();
   const [selected, setSelected] = useState(false);
   const [count, setCount] = useState(0);
+  const [refHeight, setrefHeight] = useState(0);
 
- 
+  useEffect(() => {
+    if (ref.current) {
+      setrefHeight(ref.current.scrollHeight);
+      const divHeight = document.querySelector('.div_about').style.height = refHeight +'px';
+      console.log(divHeight.height)
+    /*divHeight.style.height = refHeight;*/
+    }
+  }, [selected, refHeight]);
 
-  function handleClick() {
 
-    setCount(count +1);
+   
+
+  function handleClick(event) {
+    
+
     setSelected(!selected);
+    
   }
 
+  
+
   return (
-    <li className="listing_about">
+    <div className="listing_about">
       <div className="part_list_about">
         <h2 className="title_about">{titre}</h2>
         <img
-          className={`icon_about ${selected  ? 'active' : ''} ${!selected && count>0 ? 'inactive':""}`}
+          className={`icon_about ${selected ? 'active' : ''} ${!selected && count > 0 ? 'inactive' : ''}`}
           id={id}
           src={derouleur}
           alt="derouleur"
-          onClick={handleClick}
+          onClick={(event) => handleClick(refHeight)}
         />
       </div>
-      <div className={`div_about_supp ${!selected && count>0 ? 'closing_animation' : ''}`}>
+      <div className={`div_about_supp ${!selected && count > 0 ? 'closing_animation' : ''}`}>
         {selected && (
-          <div className="div_about">
-            {Array.isArray(paragraphe) ? (
-              paragraphe.map((paragraph, index) => (
-                <p
-                  id={id}
-                  key={index}
-                  className={`paragraphe_about ${Array.isArray(paragraphe) ? "multielement_about" : ''}`}
-                >
-                  {paragraph}
-                </p>
-              ))
-            ) : (
-              <p
-                id={id}
-                className={`paragraphe_about onlyoneelement_about ${!selected ? 'closing_animation' : ''}`}
-              >
+          <div className="div_about paragraphe_about" >
+            {paragraphe && (
+              <p id={id} className="multielement_about" ref={ref}>
                 {paragraphe}
               </p>
             )}
           </div>
         )}
       </div>
-    </li>
+    </div>
   );
 }
 
